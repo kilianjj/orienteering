8edf13d2d36ab0885b69e5e94f17b59c385bda03
Kilian Jakstis <jakstisk@gmail.com>
2024-01-22 21:40:13 -0500

    stubbed heuristic and such

--- search_util.py
+++ search_util.py
@@ -3,6 +3,11 @@ Searching logic and related functions
 Author: Kilian Jakstis
 """
 
+MAX_X = 394
+MAX_Y = 499
+
+######################## test version functions, work on ones below ####################################
+
 def dumb_search(start, end):
     path = []
     max_y = max(start[1], end[1])
@@ -31,30 +36,55 @@ def get_route(terrain, elevations, poi_path):
         end = poi_path.pop(0)
     return route
 
-# edit me
-def search(start, end):
-    visited = set()
-    backtrack = {}
-    to_visit = [start]
-    parent = None
-    while len(to_visit) != 0:
-        current = to_visit.pop(0)
-        if current in visited:  # skip over words that have already been seen
-            continue
-        if parent is None:
-            backtrack[current] = None  # mark start word's parent as None
-            parent = False
-        if current == end:
-            break
-        # generate new positions
-        # for word in new_words:
-        #     if word not in backtrack:
-        #         backtrack[word] = current  # mark new words' parent as current word
-        # to_visit.extend(new_words)
-        # visited.add(current)
-    path = []
-    head = end
-    while head is not None:  # backtrack until head is None to generate path
-        path.append(head)
-        head = backtrack.get(head)
-    return path[::-1]
+########################## real functions below ##############################
+
+# this one is good, make alterations to search function and add relevant ones
+# def get_route(terrain, elevations, poi_path):
+#     if len(poi_path) < 2:
+#         print("POI path not long enough")
+#         return
+#     route = []
+#     start = poi_path.pop(0)
+#     end = poi_path.pop(0)
+#     while True:
+#         between_points = search(start, end, terrain, elevations)
+#         route.extend(between_points)
+#         if len(poi_path) == 0:
+#             break
+#         start = end
+#         end = poi_path.pop(0)
+#     return route
+#
+# # is this fine?
+# def heuristic(coordinate, target, elevations):
+#     long_distance = (coordinate[0] - target[0]) ** 2
+#     lat_distance = (coordinate[1] - coordinate[1]) ** 2
+#     height_distance = (elevations[coordinate[0]][coordinate[1]] - elevations[target[0]][target[1]]) ** 2
+#     return (long_distance + lat_distance + height_distance) ** 1/3
+#
+# def search(start, end, terrain, elevations):
+#     visited = set()
+#     backtrack = {}
+#     to_visit = [start]
+#     parent = None
+#     while len(to_visit) != 0:
+#         current = to_visit.pop(0)
+#         if current in visited:  # skip over words that have already been seen
+#             continue
+#         if parent is None:
+#             backtrack[current] = None  # mark start word's parent as None
+#             parent = False
+#         if current == end:
+#             break
+#         # generate new positions
+#         # for word in new_words:
+#         #     if word not in backtrack:
+#         #         backtrack[word] = current  # mark new words' parent as current word
+#         # to_visit.extend(new_words)
+#         # visited.add(current)
+#     path = []
+#     head = end
+#     while head is not None:  # backtrack until head is None to generate path
+#         path.append(head)
+#         head = backtrack.get(head)
+#     return path[::-1]

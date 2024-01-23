db1185d59b0c0375286897ec5905bd4042b6de6a
Kilian Jakstis <jakstisk@gmail.com>
2024-01-23 12:04:21 -0500

    working bfs search and path drawing

--- image_util.py
+++ image_util.py
@@ -32,6 +32,24 @@ def read_image(path):
         print(f"Error reading in terrain file: {e}")
         return None
 
+# do not delete: this is the one that saves image, using show for debugging rn
+# def save_image(im, route, output_path):
+#     """
+#     Draw the ideal path on the terrain map and save in the desired location
+#     :param im: numpy array of the terrain map
+#     :param route: the ideal path (currently in tuples of coordinates***)
+#     :param output_path: path at which to save output image
+#     """
+#     try:
+#         image = Image.fromarray(im)
+#         pixels = image.load()
+#         for pixel in route:
+#             pixels[pixel[0], pixel[1]] = PATH
+#         image.save(output_path)
+#     except Exception as e:
+#         print(f"Error writing output image: {e}")
+
+# delete this one after testing
 def save_image(im, route, output_path):
     """
     Draw the ideal path on the terrain map and save in the desired location
@@ -44,7 +62,7 @@ def save_image(im, route, output_path):
         pixels = image.load()
         for pixel in route:
             pixels[pixel[0], pixel[1]] = PATH
-        image.save(output_path)
+        image.show()
     except Exception as e:
         print(f"Error writing output image: {e}")
 
--- search_util.py
+++ search_util.py
@@ -3,42 +3,23 @@ Searching logic and related functions
 Author: Kilian Jakstis
 """
 
-MAX_X = 394
-MAX_Y = 499
+MAX_X = 395
+MAX_Y = 500
 
 ######################## test version functions, work on ones below ####################################
 
-def dumb_search(start, end):
-    path = []
-    max_y = max(start[1], end[1])
-    min_y = min(start[1], end[1])
-    max_x = max(start[0], end[0])
-    min_x = min(start[0], end[0])
-    for x in range(min_x, max_x):
-        path.append((x, min_y))
-    for y in range(min_y, max_y):
-        path.append((max_x, y))
-    return path
-
-def get_route(terrain, elevations, poi_path):
-    if len(poi_path) < 2:
-        print("POI path not long enough")
-        return
-    route = []
-    start = poi_path.pop(0)
-    end = poi_path.pop(0)
-    while True:
-        between_points = dumb_search(start, end)
-        route.extend(between_points)
-        if len(poi_path) == 0:
-            break
-        start = end
-        end = poi_path.pop(0)
-    return route
-
-########################## real functions below ##############################
-
-# this one is good, make alterations to search function and add relevant ones
+# def dumb_search(start, end):
+#     path = []
+#     max_y = max(start[1], end[1])
+#     min_y = min(start[1], end[1])
+#     max_x = max(start[0], end[0])
+#     min_x = min(start[0], end[0])
+#     for x in range(min_x, max_x):
+#         path.append((x, min_y))
+#     for y in range(min_y, max_y):
+#         path.append((max_x, y))
+#     return path
+#
 # def get_route(terrain, elevations, poi_path):
 #     if len(poi_path) < 2:
 #         print("POI path not long enough")
@@ -47,44 +28,80 @@ def get_route(terrain, elevations, poi_path):
 #     start = poi_path.pop(0)
 #     end = poi_path.pop(0)
 #     while True:
-#         between_points = search(start, end, terrain, elevations)
+#         between_points = dumb_search(start, end)
 #         route.extend(between_points)
 #         if len(poi_path) == 0:
 #             break
 #         start = end
 #         end = poi_path.pop(0)
 #     return route
-#
-# # is this fine?
-# def heuristic(coordinate, target, elevations):
-#     long_distance = (coordinate[0] - target[0]) ** 2
-#     lat_distance = (coordinate[1] - coordinate[1]) ** 2
-#     height_distance = (elevations[coordinate[0]][coordinate[1]] - elevations[target[0]][target[1]]) ** 2
-#     return (long_distance + lat_distance + height_distance) ** 1/3
-#
-# def search(start, end, terrain, elevations):
-#     visited = set()
-#     backtrack = {}
-#     to_visit = [start]
-#     parent = None
-#     while len(to_visit) != 0:
-#         current = to_visit.pop(0)
-#         if current in visited:  # skip over words that have already been seen
-#             continue
-#         if parent is None:
-#             backtrack[current] = None  # mark start word's parent as None
-#             parent = False
-#         if current == end:
-#             break
-#         # generate new positions
-#         # for word in new_words:
-#         #     if word not in backtrack:
-#         #         backtrack[word] = current  # mark new words' parent as current word
-#         # to_visit.extend(new_words)
-#         # visited.add(current)
-#     path = []
-#     head = end
-#     while head is not None:  # backtrack until head is None to generate path
-#         path.append(head)
-#         head = backtrack.get(head)
-#     return path[::-1]
+
+########################## real functions below ##############################
+
+# this one is good, make alterations to search function and add relevant ones
+def get_route(terrain, elevations, poi_path):
+    if len(poi_path) < 2:
+        print("POI path not long enough")
+        return
+    route = []
+    start = poi_path.pop(0)
+    end = poi_path.pop(0)
+    while True:
+        between_points = search(start, end, terrain, elevations)
+        route.extend(between_points)
+        print(route)
+        if len(poi_path) == 0:
+            break
+        start = end
+        end = poi_path.pop(0)
+    return route
+
+# plug neighbors into this for cost function
+def distance(coordinate, target, elevations):
+    x = (coordinate[0] - target[0]) ** 2
+    y = (coordinate[1] - target[1]) ** 2
+    z = (elevations[coordinate[0]][coordinate[1]] - elevations[target[0]][target][1]) ** 2
+    return (x + y + z) ** (1/3)
+
+# is this fine? 3d distance from current point to target
+def heuristic(coordinate, target, elevations):
+    return distance(coordinate, target, elevations)
+
+# get neighbors
+def get_neighbors(coordinate):
+    neighbors = []
+    rows, cols = MAX_Y, MAX_X
+    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    for y, x in directions:
+        new_row, new_col = coordinate[1] + y, coordinate[0] + x
+        if 0 <= new_row < rows and 0 <= new_col < cols:
+            neighbors.append((new_col, new_row))
+    return neighbors
+
+# need to incorporate A* stuff
+def search(start, end, terrain, elevations):
+    visited = set()
+    backtrack = {}
+    to_visit = [start]
+    parent = None
+    while len(to_visit) != 0:
+        current = to_visit.pop(0)
+        if current in visited:  # skip over words that have already been seen
+            continue
+        if parent is None:
+            backtrack[current] = None  # mark start word's parent as None
+            parent = False
+        if current == end:
+            break
+        new_points = get_neighbors(current)
+        for point in new_points:
+            if point not in backtrack:
+                backtrack[point] = current
+        to_visit.extend(new_points)
+        visited.add(current)
+    path = []
+    head = end
+    while head is not None:  # backtrack until head is None to generate path
+        path.append(head)
+        head = backtrack.get(head)
+    return path[::-1]
